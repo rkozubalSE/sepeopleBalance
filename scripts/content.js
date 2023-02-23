5bdf1ca0ebd65a342e24955cbe1aa8990af3ead1
Richard K <richard.kozubal@secretescapes.com>
2023-02-23 17:57:13 +0100

    Keep results on page reload

--- scripts/content.js
+++ scripts/content.js
@@ -9,37 +9,36 @@ const init = () => {
         clearInterval(intervalId);
         resolve(Array.from(result));
       }
-    }, 100);
+    }, 200);
   });
   promise.then((listBalances) => {
-    console.log(11111199999);
     const listBreaks = Array.from(
       document.querySelectorAll(
         ".dataCell input:not([style*='display:none']):not([disabled])"
       )
     );
     const listTimes = Array.from(document.querySelectorAll(".dataCell select"));
-    listBreaks.forEach((elem) => {
+    const listBtns = Array.from(document.querySelectorAll(".btn"));
+    const listInteractable = [...listBreaks, ...listTimes];
+    listInteractable.forEach((elem) => {
       elem.addEventListener("change", init);
     });
-    listTimes.forEach((elem) => {
-      elem.addEventListener("change", init);
+    listBtns.forEach((elem) => {
+      elem.addEventListener("mousedown", prepForRefresh);
     });
-
     if (document.querySelector(".plop") !== null) {
       document
         .querySelector(".pbBody")
         .removeChild(document.querySelector(".plop"));
     }
-
-    const result = listBalances.reduce(calculateBalance, [0, 0]);
+    const [hours, minutes] = listBalances.reduce(calculateBalance, [0, 0]);
     const indicator = document.createElement("p");
     indicator.textContent = `⏱️ You currently have: `;
     indicator.classList.add("plop");
     const hourContainer = document.createElement("span");
-    hourContainer.textContent = `${result[0]}h and `;
+    hourContainer.textContent = `${hours}h and `;
     const minContainer = document.createElement("span");
-    minContainer.textContent = `${result[1]}min`;
+    minContainer.textContent = `${minutes}min`;
     indicator.append(hourContainer, minContainer);
     const container = document.querySelector(".list");
     container.insertAdjacentElement("beforebegin", indicator);
@@ -48,24 +47,38 @@ const init = () => {
 
 const calculateBalance = (acc, val) => {
   if (val.value !== "-8.00") {
-    const aa = val.value.split(".");
-    acc[0] += +aa[0];
-    if (aa[0].includes("-")) {
-      acc[1] -= +aa[1];
+    const [inputHours, inputMinutes] = val.value.split(".");
+    let [accHours, accMinutes] = acc;
+    accHours += +inputHours;
+    if (inputHours.includes("-")) {
+      accMinutes -= +inputMinutes;
     } else {
-      acc[1] += +aa[1];
+      accMinutes += +inputMinutes;
     }
-    if (acc[1] >= 60) {
-      acc[0] += 1;
-      acc[1] -= 60;
+    if (accMinutes >= 60) {
+      accHours += 1;
+      accMinutes -= 60;
     }
-    if (acc[1] <= -60) {
-      acc[0] -= 1;
-      acc[1] += 60;
+    if (accMinutes <= -60 || (accHours > 0 && accMinutes < 0)) {
+      accHours -= 1;
+      accMinutes += 60;
     }
-    return acc;
+    return [accHours, accMinutes];
   }
   return acc;
 };
 
 init();
+
+const prepForRefresh = () => {
+  const intervalId = setInterval(() => {
+    const listBtns = Array.from(document.querySelectorAll(".btn"));
+    const isLoading = listBtns.some((elem) => {
+      return elem.value === "Processing...";
+    });
+    if (!isLoading) {
+      clearInterval(intervalId);
+      init();
+    }
+  }, 200);
+};
